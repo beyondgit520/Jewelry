ab23d73c2c5dc9f146d595e4298f6ae1fc80ad3d
李敏 <beyondmx2@gmail.com>
2016-07-20 09:47:06 +0800

    databinding vector

--- app/src/main/java/com/app/base/APP.java
+++ app/src/main/java/com/app/base/APP.java
@@ -1,6 +1,7 @@
 package com.app.base;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 
@@ -8,9 +9,16 @@ import com.facebook.drawee.backends.pipeline.Fresco;
  * Created by Administrator on 2016/7/6.
  */
 public class APP extends Application {
+    private static Context context;
+
+    public static Context getContext() {
+        return context;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
         Fresco.initialize(this);
+        context = getApplicationContext();
     }
 }
--- app/src/main/java/com/app/base/BaseActivity.java
+++ app/src/main/java/com/app/base/BaseActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.util.DisplayMetrics;
 
 /**
@@ -15,7 +16,9 @@ public class BaseActivity extends AppCompatActivity {
     protected String tag;
     public static DisplayMetrics displayMetrics;
     protected ProgressDialog progressDialog;
-
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
